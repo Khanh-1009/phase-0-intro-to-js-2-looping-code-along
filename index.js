1f9a1a772ed9eba3cb478ed704ab981e8ed0b638
Khanh Tran <katetrannbkhanh@gmail.com>
2023-12-02 00:18:19 -0800

    commit tree 76a7c58fd68d6fbb5aff07c3fa5fe096c849a858

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
+function writeCards(strName, eveName){
+    const newArray = []
+    for (let i = 0; i < strName.length; i += 1) {
+        newArray.push(`Thank you, ${strName[i]}, for the wonderful ${eveName} gift!`)
+    }
+    return newArray
+}
